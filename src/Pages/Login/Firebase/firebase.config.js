504309ec436b16930ff4ffdc0b001af1b1fe0cb1
jahanara khatun <zarakhatun58@gmail.com>
2021-11-20 19:32:39 +0530

    finished

--- src/Pages/Login/Firebase/firebase.config.js
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -1,15 +1,31 @@
 const firebaseConfig = {
-    apiKey:process.env.REACT_APP_API_KEY,
-    authDomain:process.env.REACT_APP_AUTH_DOMAIN,
-    projectId:process.env.REACT_APP_PROJECT_ID,
-    storageBucket:process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId:process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId:process.env.REACT_APP_APP_ID
+    // apiKey:process.env.REACT_APP_API_KEY,
+    // authDomain:process.env.REACT_APP_AUTH_DOMAIN,
+    // projectId:process.env.REACT_APP_PROJECT_ID,
+    // storageBucket:process.env.REACT_APP_STORAGE_BUCKET,
+    // messagingSenderId:process.env.REACT_APP_MESSAGING_SENDER_ID,
+    // appId:process.env.REACT_APP_APP_ID
     
-
+    
+    
+    apiKey: "AIzaSyBJMkX3uhUgdwKnviItIh-5sA9S3IEjNpk",
+    authDomain: "doctors-portal-ae96f.firebaseapp.com",
+    projectId: "doctors-portal-ae96f",
+    storageBucket: "doctors-portal-ae96f.appspot.com",
+    messagingSenderId: "683917265704",
+    appId: "1:683917265704:web:ab09a256ce9b8155f95e1d"
+    
     
   };
   
   export default firebaseConfig;
   
+   /*
+     apiKey: "AIzaSyBJMkX3uhUgdwKnviItIh-5sA9S3IEjNpk",
+  authDomain: "doctors-portal-ae96f.firebaseapp.com",
+  projectId: "doctors-portal-ae96f",
+  storageBucket: "doctors-portal-ae96f.appspot.com",
+  messagingSenderId: "683917265704",
+  appId: "1:683917265704:web:ab09a256ce9b8155f95e1d"
   
+  */
